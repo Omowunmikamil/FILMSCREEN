99db973e79e77371e64062648000144e5c35ab09
Omowunmi Kamiludeen <balikiskamil@gmail.com>
2023-09-19 13:39:43 +0100

    Consumed API's for landing page

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
 // Using the TMDB API to get the movie data
 
-const API_KEY = 'c6e4f8f45ddef6fbd022a88ecb99fc80';
-const URL_BASE = 'https://api.themoviedb.org/3';
-const API_URL = URL_BASE + '/discover/movie?sort_by=popularity.desc&api_key=' + API_KEY;
+const API_KEY = '2a484bae29615b049d11c8864b9d76ea';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&api_key=' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
 
-getMovieDB(API_URL);
+const main = document.getElementById('main');
 
-function getMovieDB(url) {
+getMovies(API_URL);
+
+function getMovies(url) {
+    
     fetch(url).then(res => res.json()).then(data=> {
-        console.log(data);
+        console.log(data.results);
+        showMovies(data.results);
     })
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+            <img src="${IMG_URL + poster_path}" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getcolor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+                <h1>Overview</h1>
+                ${overview}
+            </div>
+        `
+        main.appendChild(movieElement);
+    })
+}
+
+function getcolor(vote) {
+    if(vote >= 8) {
+        return 'lightgreen';
+    } else if(vote >= 5) {
+        return 'yellow';
+    } else {
+        return 'red';
+    }
 }
